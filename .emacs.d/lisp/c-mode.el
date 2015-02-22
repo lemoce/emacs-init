cc6ea0c450859593cb1d95604f4148d6b79dad3b
lemoce <cerencio@yahoo.com.br>
2015-02-22 20:29:52 -0300

    Update c-mode.el

--- .emacs.d/lisp/c-mode.el
+++ .emacs.d/lisp/c-mode.el
@@ -1,23 +1,24 @@
 (custom-set-variables
- '(company-c-headers-path-system
-   (quote
-    ("/usr/include/" "/usr/local/include/" "/Applications/Xcode.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX10.10.sdk/usr/include" "/Applications/Xcode.app/Contents/Developer/Toolchains/XcodeDefault.xctoolchain/usr/bin/../include/c++/v1" "/Applications/Xcode.app/Contents/Developer/Toolchains/XcodeDefault.xctoolchain/usr/bin/../lib/clang/6.0/include" "/Applications/Xcode.app/Contents/Developer/Toolchains/XcodeDefault.xctoolchain/usr/include"))))
-
+ '(c-default-style (quote ((java-mode . "java") (awk-mode . "awk") (other . "gnu"))))
+ '(company-c-headers-path-system (quote ("/usr/include/" "/usr/local/include/" "/usr/include/c++/4.8.2" "/usr/include/c++/4.8.2/x86_64-redhat-linux" "/usr/include/c++/4.8.2/backward" "/usr/lib/clang/3.4.2/include")))
+ '(company-idle-delay 0))
 
 (add-to-list 'auto-mode-alist '("\\.h\\'" . c++-mode))
 
-(defun my:flymake-google-init ()
-  (require 'flymake-google-cpplint)
-  (custom-set-variables
-   '(flymake-google-cpplint-command "/usr/local/bin/cpplint"))
-  (flymake-google-cpplint-load))
+;; (defun my:flymake-google-init ()
+;;   (require 'flymake-google-cpplint)
+;;   (custom-set-variables
+;;    '(flymake-google-cpplint-command "/usr/local/bin/cpplint"))
+;;   (flymake-google-cpplint-load))
+;; (add-hook 'c-mode-hook 'my:flymake-google-init)
+;; (add-hook 'c++-mode-hook 'my:flymake-google-init)
 
-(add-hook 'c-mode-hook 'my:flymake-google-init)
-(add-hook 'c++-mode-hook 'my:flymake-google-init)
+(add-hook 'c-mode-hook 'flycheck-mode)
+(add-hook 'c++-mode-hook 'flycheck-mode)
 
-(require 'google-c-style)
-(add-hook 'c-mode-common-hook 'google-set-c-style)
-(add-hook 'c-mode-common-hook 'google-make-newline-indent)
+;; (require 'google-c-style)
+;; (add-hook 'c-mode-common-hook 'google-set-c-style)
+;; (add-hook 'c-mode-common-hook 'google-make-newline-indent)
 
 (setenv "LD_LIBRARY_PATH" "/Applications/Xcode.app/Contents/Developer/Toolchains/XcodeDefault.xctoolchain/usr/lib/")
 
@@ -39,16 +40,14 @@
 (add-hook 'after-init-hook 'global-company-mode)
 (add-to-list 'company-backends 'company-c-headers)
 (add-to-list 'company-backends 'company-irony)
+(add-to-list 'company-backends 'company-clang)
 (add-to-list 'company-backends 'company-cmake)
-
-(setq company-cmake-executable "/usr/local/bin/cmake")
-
+(setq company-cmake-executable "/usr/bin/cmake")
 ;; (optional) adds CC special commands to `company-begin-commands' in order to
 ;; trigger completion at interesting places, such as after scope operator
 ;; std::|
 (add-hook 'irony-mode-hook 'company-irony-setup-begin-commands)
 ;;(global-set-key "\t" 'company-complete-common)
-
 (define-key company-mode-map (kbd "C-,") 'company-clang)
 (define-key company-mode-map (kbd "C-<") 'company-c-headers)
 (define-key company-mode-map (kbd "C-.") 'company-complete-common)
