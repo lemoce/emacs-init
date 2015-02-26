52a1e3d475bb2e57ad6c5d816ccf53a96d08b897
lemoce <cerencio@yahoo.com.br>
2015-02-25 23:12:20 -0300

    Update auto-complete-mode.el

--- .emacs.d/lisp/auto-complete-mode.el
+++ .emacs.d/lisp/auto-complete-mode.el
@@ -9,23 +9,19 @@
 
 (define-key ac-mode-map (kbd "C-,") 'auto-complete)
 
-(defun my:ac-config ()
-  (setq-default ac-sources '(ac-source-abbrev
-			     ac-source-dictionary
-			     ac-source-words-in-same-mode-buffers))
-  (add-hook 'emacs-lisp-mode-hook 'ac-emacs-lisp-mode-setup)
-  (add-hook 'auto-complete-mode-hook 'ac-common-setup)
-  (global-auto-complete-mode t))
+(setq-default ac-sources '(ac-source-abbrev
+			   ac-source-dictionary
+			   ac-source-words-in-same-mode-buffers))
+
+(global-auto-complete-mode t)
 
 (defun my:ac-cc-mode-config ()
   (setq ac-sources (append '(ac-source-clang
-			     ac-source-yasnippet) ac-sources)))
-(add-hook 'c-mode-common-hook 'my:ac-cc-mode-config)
-
-(setq ac-clang-flags
-      (mapcar (lambda (item)(concat "-I" item))
-	       (split-string
-		"
+			     ac-source-yasnippet) ac-sources)
+	ac-clang-flags
+	(mapcar (lambda (item)(concat "-I" item))
+		(split-string
+		 "
 /Applications/Xcode.app/Contents/Developer/Toolchains/XcodeDefault.xctoolchain/usr/include/c++/v1
 /Applications/Xcode.app/Contents/Developer/Toolchains/XcodeDefault.xctoolchain/usr/lib/clang/6.0/include
 /Applications/Xcode.app/Contents/Developer/Toolchains/XcodeDefault.xctoolchain/usr/include
@@ -33,6 +29,14 @@
 /Applications/Xcode.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX10.10.sdk/System/Library/Frameworks
 /usr/local/include
 "
-		)))
+		 ))))
+(add-hook 'c-mode-common-hook 'my:ac-cc-mode-config)
 
-(my:ac-config)
+(add-to-list 'ac-modes 'lisp-mode)
+(add-to-list 'ac-modes 'c-mode)
+(add-to-list 'ac-modes 'c++-mode)
+
+(defun auto-complete-mode-maybe ()
+  "No maybe for you. Only AC!"
+  (unless (minibufferp (current-buffer))
+    (auto-complete-mode 1)))
