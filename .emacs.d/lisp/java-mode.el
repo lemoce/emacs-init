fa39227749c4616c6bdb4c790746bbe40bea65c1
lemoce <cerencio@yahoo.com.br>
2015-02-26 19:10:51 -0300

    Update java-mode.el

--- .emacs.d/lisp/java-mode.el
+++ .emacs.d/lisp/java-mode.el
@@ -2,15 +2,16 @@
 (require 'eclimd)
 (require 'eclim-maven)
 
-(custom-set-variables
-  '(eclim-eclipse-dirs '("~/apps/eclipse-javaee-luna"))
-  '(eclim-executable "~/apps/eclipse-javaee-luna/eclim")
-  '(eclimd-executable "~/apps/eclipse-javaee-luna/eclimd")
-  '(eclimd-default-workspace "~/eclim-workspace"))
-
-(require 'company)
-(require 'company-emacs-eclim)
-(company-emacs-eclim-setup)
+(require 'ac-emacs-eclim-source)
+(ac-emacs-eclim-config)
 
 (add-hook 'java-mode-hook 'eclim-mode)
 (add-hook 'java-mode-hook 'company-mode)
+
+(defun my:custom-varibles-java-mode ()
+  (setq eclim-eclipse-dirs '("~/apps/eclipse-javaee-luna")
+	eclim-executable "~/apps/eclipse-javaee-luna/eclim"
+	eclimd-executable "~/apps/eclipse-javaee-luna/eclimd"
+	eclimd-default-workspace "~/eclim-workspace"))
+
+(add-hook 'java-mode-hook 'my:custom-varibles-java-mode)
