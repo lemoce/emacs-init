29fd70705c86ab5f7c8524e6aa082db4a03106c6
lemoce <cerencio@yahoo.com.br>
2015-02-26 19:11:49 -0300

    Update auto-complete-mode.el

--- .emacs.d/lisp/auto-complete-mode.el
+++ .emacs.d/lisp/auto-complete-mode.el
@@ -13,8 +13,6 @@
 			   ac-source-dictionary
 			   ac-source-words-in-same-mode-buffers))
 
-(global-auto-complete-mode t)
-
 (defun my:ac-cc-mode-config ()
   (setq ac-sources (append '(ac-source-clang
 			     ac-source-yasnippet) ac-sources)
@@ -22,12 +20,13 @@
 	(mapcar (lambda (item)(concat "-I" item))
 		(split-string
 		 "
-/Applications/Xcode.app/Contents/Developer/Toolchains/XcodeDefault.xctoolchain/usr/include/c++/v1
-/Applications/Xcode.app/Contents/Developer/Toolchains/XcodeDefault.xctoolchain/usr/lib/clang/6.0/include
-/Applications/Xcode.app/Contents/Developer/Toolchains/XcodeDefault.xctoolchain/usr/include
-/Applications/Xcode.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX10.10.sdk/usr/include
-/Applications/Xcode.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX10.10.sdk/System/Library/Frameworks
+/usr/include/c++/4.9
+/usr/include/i386-linux-gnu/c++/4.9
+/usr/include/c++/4.9/backward
 /usr/local/include
+/home/27190057897/local/llvm/lib/clang/3.6.0/include
+/usr/include/i386-linux-gnu
+/usr/include
 "
 		 ))))
 (add-hook 'c-mode-common-hook 'my:ac-cc-mode-config)
@@ -35,8 +34,11 @@
 (add-to-list 'ac-modes 'lisp-mode)
 (add-to-list 'ac-modes 'c-mode)
 (add-to-list 'ac-modes 'c++-mode)
+(add-to-list 'ac-modes 'java-mode)
 
 (defun auto-complete-mode-maybe ()
   "No maybe for you. Only AC!"
   (unless (minibufferp (current-buffer))
     (auto-complete-mode 1)))
+
+(global-auto-complete-mode t)
